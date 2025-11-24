4b4d303d6db545283fb159e0a909a044ab6e3e25
hassemer-g <g.hassemer@protonmail.com>
2025-11-23 20:51:15 -0400

    Update val.js

--- src/val.js
+++ src/val.js
@@ -1,29 +1,23 @@
 
-export function valAccountNameStructure(accountName) {
+export function valStringCharSet(
+    input,
+    charSet,
+) {
 
     if (
-        [accountName].some(v => typeof v !== "string" || !v.trim())
+        [input, charSet].some(v => typeof v !== "string" || !v.trim())
     ) {
+
         return false;
     }
 
-    const accountNameRegex = /^[a-z](([a-z0-9-]+)?[a-z0-9])?(\.[a-z](([a-z0-9-]+)?[a-z0-9])?)*$/;
+    const allowed = new Set(charSet);
 
-    const validDotPlacement = !accountName.includes(".") || accountName.split(".").every(part => part.length >= 3);
+    for (const char of input) {
+        if (!allowed.has(char)) {
+            return false;
+        }
+    }
 
-    const noDotFollowedByHyphenOrNumber = !/\.[0-9-]/.test(accountName);
-
-    return (
-        !(accountName.length < 3)
-        && !(accountName.length > 16)
-        && accountNameRegex.test(accountName)
-        && validDotPlacement
-        && noDotFollowedByHyphenOrNumber
-        && !accountName.includes("-.")
-    );
-}
-
-export function valHivePrivKey(privKey) {
-    const privKeyRegex = /^5[1-9A-HJ-NP-Za-km-z]{50}$/;
-    return typeof privKey === "string" && privKey.length === 51 && privKeyRegex.test(privKey);
+    return true;
 }
