dfc6025df8e9accdbf82d93db35ad4b32f794fd4
hassemer-g <g.hassemer@protonmail.com>
2025-11-23 20:27:07 -0400

    Update encryptPage.js

--- src/encryptPage.js
+++ src/encryptPage.js
@@ -1,14 +1,22 @@
 import {
     createSHA512,
     createSHA3,
+    createWhirlpool,
     createBLAKE2b,
     createBLAKE3,
-    createWhirlpool,
-    createXXHash128,
+    createSM3,
 } from "./hash-wasm/hash-wasm.mjs";
 import {
     utf8ToBytes,
 } from "./utils.js";
+import {
+    encodeBase91,
+    decodeBase91,
+} from "./base91.js";
+import {
+    customBase91CharSet,
+} from "./charsets.js";
+import { valStringCharSet } from "./val.js";
 import { RPCsArray } from "./rpcs.js";
 import { testRPCsWithDhive } from "./test_rpcs.js";
 import {
@@ -33,10 +41,10 @@ if (!testedRPCs || !testedRPCs.length) {
 const Hs = {
     sha2: await createSHA512(),
     sha3: await createSHA3(),
+    whirlpool: await createWhirlpool(),
     blake2: await createBLAKE2b(),
     blake3: await createBLAKE3(),
-    whirlpool: await createWhirlpool(),
-    xxhash: await createXXHash128(),
+    sm3: await createSM3(),
 };
 
 const addresseeInput = document.getElementById("addresseeEnc");
@@ -90,46 +98,78 @@ usePQ.addEventListener("change", () => {
 
 encryptButton.addEventListener("click", async () => {
 
-    const t = addresseeInput.value.trim();
+    const addressee = addresseeInput.value.trim();
 
-    let recipientPubHMkey = null;
+    let recipientPubHMkey;
     try {
 
         recipientPubHMkey = await fetchPubKey(
-            t,
+            addressee,
             testedRPCs,
         );
     } catch (err) {
-        resultMessage.textContent = `Failed to get the metadata from account "${t}"!`;
+        resultMessage.textContent = `Failed to get the metadata from account "${addressee}"!`;
         resultMessage.style.color = "red";
+        return;
     }
 
-    if (recipientPubHMkey && recipientPubHMkey instanceof Uint8Array) {
+    if (recipientPubHMkey && recipientPubHMkey instanceof Uint8Array && recipientPubHMkey.length) {
 
-        const msgStr = await encryptMsg(
-            utf8ToBytes(plaintextInput.value.trim()),
-            t,
+        const plaintext = plaintextInput.value.trim();
+        let plaintextBytes, fileEncInput = false;
+        if (
+            plaintext.startsWith(`"F"`)
+            && plaintext.endsWith(`"`)
+            && valStringCharSet(plaintext.slice(3, -1), customBase91CharSet)
+        ) {
+            plaintextBytes = decodeBase91(plaintext.slice(3, -1);
+            fileEncInput = true;
+        } else {
+            plaintextBytes = utf8ToBytes(plaintext);
+        }
+
+        const toUsePq = usePQ.checked;
+
+        const payload = await encryptMsg(
+            plaintextBytes,
+            addressee,
             recipientPubHMkey,
             Hs,
-            usePQ.checked ? false : true,
+            toUsePq ? false : true,
         );
 
+        let msgStr;
+        if (fileEncInput) {
+            if (toUsePq) {
+                msgStr = `"0MF"${encodeBase91(payload)}"`;
+            } else {
+                msgStr = `"0mF"${encodeBase91(payload)}"`;
+            }
+        } else {
+            if (toUsePq) {
+                msgStr = `"0M"${encodeBase91(payload)}"`;
+            } else {
+                msgStr = `"0m"${encodeBase91(payload)}"`;
+            }
+        }
+
         if (
-            typeof msgStr === "string" && msgStr.trim()
+            typeof msgStr === "string"
+            && msgStr.trim()
         ) {
 
         resultMessage.textContent = `Message successfully encrypted!`;
         resultMessage.style.color = "green";
         copyButtonEnc.disabled = false;
         copyButtonEnc.style.backgroundColor = "darkorange";
-        ENCRYPTED_MSG = `"${msgStr}"`;
+        ENCRYPTED_MSG = msgStr;
 
         } else {
             resultMessage.textContent = `Failed to encrypt message!`;
             resultMessage.style.color = "red";
         }
     } else {
-        resultMessage.textContent = `The account ${t} does not have a Hive-Mail public key registered onchain!`;
+        resultMessage.textContent = `The account ${addressee} does not have a Hive-Mail public key registered onchain!`;
         resultMessage.style.color = "red";
     }
 });
