6eeb82975da89b256f8596ecb5dbb9a48acc9c23
hassemer-g <g.hassemer@protonmail.com>
2025-11-23 20:30:31 -0400

    Update xsalsa-poly1305.js

--- src/xsalsa-poly1305.js
+++ src/xsalsa-poly1305.js
@@ -6,16 +6,6 @@ export function encryptXSalsaPoly1305(
     nonce,
 ) {
 
-    if (!(plaintext instanceof Uint8Array)) {
-        throw new Error("Plaintext is supposed to be a Uint8Array.");
-    }
-    if (!(key instanceof Uint8Array) || key.length !== 32) {
-        throw new Error("Key is supposed to be 32-byte Uint8Array.");
-    }
-    if (!(nonce instanceof Uint8Array) || nonce.length !== 24) {
-        throw new Error("Nonce is supposed to be 24-byte Uint8Array.");
-    }
-
     const cipher = xsalsa20poly1305(key, nonce);
 
     return cipher.encrypt(plaintext);
@@ -26,16 +16,6 @@ export function decryptXSalsaPoly1305(
     key,
     nonce,
 ) {
-
-    if (!(ciphertext instanceof Uint8Array)) {
-        throw new Error("Ciphertext is supposed to be a Uint8Array.");
-    }
-    if (!(key instanceof Uint8Array) || key.length !== 32) {
-        throw new Error("Key is supposed to be 32-byte Uint8Array.");
-    }
-    if (!(nonce instanceof Uint8Array) || nonce.length !== 24) {
-        throw new Error("Nonce is supposed to be 24-byte Uint8Array.");
-    }
 
     const cipher = xsalsa20poly1305(key, nonce);
 
