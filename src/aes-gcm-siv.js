71b5460158f672add45dbf5685d0e898dc426764
hassemer-g <g.hassemer@protonmail.com>
2025-11-23 20:39:27 -0400

    Update aes-gcm-siv.js

--- src/aes-gcm-siv.js
+++ src/aes-gcm-siv.js
@@ -6,16 +6,6 @@ export function encryptAesGcmSiv(
     nonce,
 ) {
 
-    if (!(plaintext instanceof Uint8Array)) {
-        throw new Error("Plaintext is supposed to be a Uint8Array.");
-    }
-    if (!(key instanceof Uint8Array) || key.length !== 32) {
-        throw new Error("Key is supposed to be 32-byte Uint8Array.");
-    }
-    if (!(nonce instanceof Uint8Array) || nonce.length !== 12) {
-        throw new Error("Nonce is supposed to be 12-byte Uint8Array.");
-    }
-
     const cipher = gcmsiv(key, nonce);
 
     return cipher.encrypt(plaintext);
@@ -26,16 +16,6 @@ export function decryptAesGcmSiv(
     key,
     nonce,
 ) {
-
-    if (!(ciphertext instanceof Uint8Array)) {
-        throw new Error("Ciphertext is supposed to be a Uint8Array.");
-    }
-    if (!(key instanceof Uint8Array) || key.length !== 32) {
-        throw new Error("Key is supposed to be 32-byte Uint8Array.");
-    }
-    if (!(nonce instanceof Uint8Array) || nonce.length !== 12) {
-        throw new Error("Nonce is supposed to be 12-byte Uint8Array.");
-    }
 
     const cipher = gcmsiv(key, nonce);
 
