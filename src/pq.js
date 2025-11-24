bbbc5068a663bde00d970531e23f4e4e80ab61fd
hassemer-g <g.hassemer@protonmail.com>
2025-11-23 20:34:40 -0400

    Update pq.js

--- src/pq.js
+++ src/pq.js
@@ -25,13 +25,6 @@ export async function createPQkeyPair(
     const privKey = new Uint8Array(privateKey.export());
     const pubKey = new Uint8Array(publicKey.export());
 
-    if (
-        !valPrivPQkey(privKey, algorithm)
-        || !valPubPQkey(pubKey, algorithm)
-    ) {
-        throw new Error(`Function "createPQkeyPair" failed!`);
-    }
-
     return { privKey, pubKey };
 }
 
@@ -40,26 +33,10 @@ export function extractPQpubKey(
     algorithm,
 ) {
 
-    if (
-        !valPrivPQkey(privKey, algorithm)
-    ) {
-        throw new Error(`Invalid private key input to the "extractPQpubKey" function!`);
-    }
-
     const pubKeyLength = algorithm === "ml-kem-1024" ? 1568 : algorithm === "hqc-256" ? 7245 : NaN;
     const pubKeyOffset = algorithm === "ml-kem-1024" ? 1536 : algorithm === "hqc-256" ? 72 : NaN;
 
-    if (pubKeyLength === NaN || pubKeyOffset === NaN) {
-        throw new Error(`Invalid algorithm input to the "extractPQpubKey" function!`);
-    }
-
     const pubKey = privKey.slice(pubKeyOffset, pubKeyOffset + pubKeyLength);
-
-    if (
-        !valPubPQkey(pubKey, algorithm)
-    ) {
-        throw new Error(`Function "extractPQpubKey" failed!`);
-    }
 
     return pubKey;
 }
@@ -68,12 +45,6 @@ export async function buildPQsharedSecret(
     pubKey,
     algorithm,
 ) {
-
-    if (
-        !valPubPQkey(pubKey, algorithm)
-    ) {
-        throw new Error(`Invalid input to the "buildPQsharedSecret" function!`);
-    }
 
     const pqPub = new pqclean.kem.PublicKey(algorithm, pubKey);
     const { key, encryptedKey } = await pqPub.generateKey();
@@ -88,13 +59,6 @@ export async function retrievePQsharedSecret(
     encryptedKey,
     algorithm,
 ) {
-
-    if (
-        !valPrivPQkey(privKey, algorithm)
-        || !(encryptedKey instanceof Uint8Array)
-    ) {
-        throw new Error(`Invalid inputs to the "retrievePQsharedSecret" function!`);
-    }
 
     const pqPriv = new pqclean.kem.PrivateKey(algorithm, privKey);
     const key = await pqPriv.decryptKey(encryptedKey);
