adc2892e9bd1b1141d48f06962a8e77d22b8a5ea
hassemer-g <g.hassemer@protonmail.com>
2025-11-26 11:41:31 -0400

    Update hm-encrypt.js

--- src/hm-encrypt.js
+++ src/hm-encrypt.js
@@ -41,10 +41,11 @@ export async function encryptMsg(
     }
 
     const keypairs = doHashing(
-        concatBytes(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret, utf8ToBytes(`ჰM0 ${recipientName} ${recipientPubX25519Key.length} ${x25519Ephemeral.length} ${x25519SharedSecret.length} ${recipientPubKyberKey.length} ${kyberEphemeral.length} ${kyberSharedSecret.length} ${recipientPubHQCkey.length} ${hqcEphemeral.length} ${hqcSharedSecret.length} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ჰ`), recipientPubX25519Key, x25519Ephemeral, recipientPubKyberKey, kyberEphemeral, recipientPubHQCkey, hqcEphemeral),
+        concatBytes(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret, utf8ToBytes(`ჰM0 ${recipientName}      ${recipientPubX25519Key.length} ${x25519Ephemeral.length} ${x25519SharedSecret.length} ${recipientPubKyberKey.length} ${kyberEphemeral.length} ${kyberSharedSecret.length} ${recipientPubHQCkey.length} ${hqcEphemeral.length} ${hqcSharedSecret.length} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ჰ`), recipientPubX25519Key, x25519Ephemeral, recipientPubKyberKey, kyberEphemeral, recipientPubHQCkey, hqcEphemeral),
         Hs,
         [24, 12, 24, 32, 32, 32],
         1000,
+        true,
     );
     wipeUint8(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret);
 
