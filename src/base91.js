cbe0b466b942cfe823849eaf7776b4ab2108a6b5
hassemer-g <g.hassemer@protonmail.com>
2026-01-10 18:06:09 -0400

    Update base91.js

--- src/base91.js
+++ src/base91.js
@@ -3,82 +3,82 @@ import {
 } from "./charsets.js";
 
 export function encodeBase91(
-    data,
+    d,
 ) {
+    const c = customBase91CharSet;
+    const l = d.length;
+    let R = "", b = 0, n = 0;
 
-    const len = data.length;
-    let ret = "";
-
-    let n = 0;
-    let b = 0;
-
-    for (let i = 0; i < len; i++) {
-        b |= data[i] << n;
+    for (let i = 0; i < l; i++) {
+        b |= d[i] << n;
         n += 8;
 
-        if (n > 13) {
+        if (n >= 14) {
             let v = b & 8191;
 
             if (v > 88) {
                 b >>= 13;
                 n -= 13;
-
             } else {
                 v = b & 16383;
                 b >>= 14;
                 n -= 14;
             }
 
-            ret += customBase91CharSet[v % 91] + customBase91CharSet[v / 91 | 0];
+            const o = v % 91;
+            R += c[o] + c[(v - o) / 91];
         }
     }
 
     if (n) {
-        ret += customBase91CharSet[b % 91];
+        R += c[b % 91];
 
-        if (n > 7 || b > 90) ret += customBase91CharSet[b / 91 | 0];
+        if (n > 7 || b > 90) {
+            R += c[(b - (b % 91)) / 91];
+        }
     }
 
-    return ret;
+    return R;
 }
 
 export function decodeBase91(
-    data,
+    d,
 ) {
+    const c = customBase91CharSet;
+    const l = d.length;
+    const R = [];
+    let o = 0, b = 0, n = 0, v = -1;
 
-    const len = data.length;
-    const ret = [];
+    const r = new Int16Array(256);
+    r.fill(-1);
+    for (let i = 0; i < 91; i++) {
+        r[c.charCodeAt(i)] = i;
+    }
 
-    let b = 0;
-    let n = 0;
-    let v = -1;
-
-    for (let i = 0; i < len; i++) {
-        const p = customBase91CharSet.indexOf(data[i]);
-
+    for (let i = 0; i < l; i++) {
+        const p = r[d.charCodeAt(i)];
         if (p === -1) continue;
 
         if (v < 0) {
             v = p;
-
         } else {
             v += p * 91;
             b |= v << n;
             n += (v & 8191) > 88 ? 13 : 14;
 
-            do {
-                ret.push(b & 0xff);
+            while (n >= 8) {
+                R[o++] = b & 0xff;
                 b >>= 8;
                 n -= 8;
-            } while (n > 7);
+            }
 
             v = -1;
         }
     }
 
     if (v > -1) {
-        ret.push((b | v << n) & 0xff);
+        R[o++] = (b | (v << n)) & 0xff;
     }
 
-    return new Uint8Array(ret);
+    return new Uint8Array(R);
 }
