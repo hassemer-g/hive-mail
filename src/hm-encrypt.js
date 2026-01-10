4572227f0f9059945c1cf92f1780b9f2720731d1
hassemer-g <g.hassemer@protonmail.com>
2026-01-10 18:02:25 -0400

    Update hm-encrypt.js

--- src/hm-encrypt.js
+++ src/hm-encrypt.js
@@ -1,7 +1,7 @@
 import {
-    concatBytes,
+    concatUint8Arr,
     utf8ToBytes,
-    wipeUint8,
+    wipeUint8Arr,
 } from "./utils.js";
 import {
     doHashing,
@@ -26,11 +26,18 @@ export async function encryptMsg(
     recipientPubHMkey,
     Hs,
     doNotUsePq = false,
+    inputIsFile = false,
 ) {
+    const HM_version = "000";
+    let HM_mode;
+    if (!doNotUsePq & !inputIsFile) { HM_mode = "0000" }
+    else if (doNotUsePq & !inputIsFile) { HM_mode = "0001" }
+    else if (!doNotUsePq & inputIsFile) { HM_mode = "0002" }
+    else if (doNotUsePq & inputIsFile) { HM_mode = "0003" }
 
-    const recipientPubX25519Key = recipientPubHMkey.slice(0, 32);
-    const recipientPubKyberKey = recipientPubHMkey.slice(32, 1600);
-    const recipientPubHQCkey = recipientPubHMkey.slice(1600);
+    const recipientPubX25519Key = recipientPubHMkey.subarray(0, 32);
+    const recipientPubKyberKey = recipientPubHMkey.subarray(32, 1600);
+    const recipientPubHQCkey = recipientPubHMkey.subarray(1600);
 
     const { sharedSecret: x25519SharedSecret, encryptedSharedSecret: x25519Ephemeral } = buildX25519SharedSecret(recipientPubX25519Key);
 
@@ -41,20 +48,20 @@ export async function encryptMsg(
     }
 
     const keypairs = doHashing(
-        concatBytes(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret, utf8ToBytes(`ჰM0 ${recipientName}      ${recipientPubX25519Key.length} ${x25519Ephemeral.length} ${x25519SharedSecret.length} ${recipientPubKyberKey.length} ${kyberEphemeral.length} ${kyberSharedSecret.length} ${recipientPubHQCkey.length} ${hqcEphemeral.length} ${hqcSharedSecret.length} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ჰ`), recipientPubX25519Key, x25519Ephemeral, recipientPubKyberKey, kyberEphemeral, recipientPubHQCkey, hqcEphemeral),
+        concatUint8Arr(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret, utf8ToBytes(`ჰM-${HM_version} ${HM_mode} ${recipientName} ${recipientPubX25519Key.length} ${x25519Ephemeral.length} ${x25519SharedSecret.length} ${recipientPubKyberKey.length} ${kyberEphemeral.length} ${kyberSharedSecret.length} ${recipientPubHQCkey.length} ${hqcEphemeral.length} ${hqcSharedSecret.length} ჰ`), recipientPubX25519Key, x25519Ephemeral, recipientPubKyberKey, kyberEphemeral, recipientPubHQCkey, hqcEphemeral),
         Hs,
         [24, 12, 24, 32, 32, 32],
         1000,
         true,
     );
-    wipeUint8(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret);
+    wipeUint8Arr(x25519SharedSecret, kyberSharedSecret, hqcSharedSecret);
 
     const ciphertext1 = encryptXChaCha20Poly1305(
         plaintext,
         keypairs[5],
         keypairs[2],
     );
-    wipeUint8(plaintext);
+    wipeUint8Arr(plaintext);
 
     let finalCiphertext;
     if (doNotUsePq) {
@@ -75,7 +82,15 @@ export async function encryptMsg(
         );
     }
 
-    return concatBytes(
+    const prefixNumber = Number(HM_version + HM_mode + (String(finalCiphertext.length).slice(-3)).padStart(3, "0"));
+
+    return concatUint8Arr(
+        Uint8Array.of(
+            prefixNumber & 0xff,
+            (prefixNumber >>> 8) & 0xff,
+            (prefixNumber >>> 16) & 0xff,
+            (prefixNumber >>> 24) & 0xff,
+        ),
         x25519Ephemeral,
         kyberEphemeral,
         hqcEphemeral,
