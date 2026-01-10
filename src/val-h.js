656f0aa7776ad86020c60a9a7add60502c95442f
hassemer-g <g.hassemer@protonmail.com>
2026-01-10 17:57:16 -0400

    Update val-h.js

--- src/val-h.js
+++ src/val-h.js
@@ -1,29 +1,16 @@
 
-export function valAccountNameStructure(accountName) {
-
-    if (
-        [accountName].some(v => typeof v !== "string" || !v.trim())
-    ) {
-        return false;
-    }
-
-    const accountNameRegex = /^[a-z](([a-z0-9-]+)?[a-z0-9])?(\.[a-z](([a-z0-9-]+)?[a-z0-9])?)*$/;
-
-    const validDotPlacement = !accountName.includes(".") || accountName.split(".").every(part => part.length >= 3);
-
-    const noDotFollowedByHyphenOrNumber = !/\.[0-9-]/.test(accountName);
-
-    return (
-        !(accountName.length < 3)
-        && !(accountName.length > 16)
-        && accountNameRegex.test(accountName)
-        && validDotPlacement
-        && noDotFollowedByHyphenOrNumber
-        && !accountName.includes("-.")
-    );
+export function valAccountNameStructure(accName) {
+    return typeof accName === "string"
+        && accName.length > 2
+        && accName.length < 17
+        && /^[a-z](([a-z0-9-]+)?[a-z0-9])?(\.[a-z](([a-z0-9-]+)?[a-z0-9])?)*$/.test(accName)
+        && (!accName.includes(".") || accName.split(".").every(p => p.length > 2))
+        && !/\.[0-9-]/.test(accName)
+        && !accName.includes("-.");
 }
 
 export function valHivePrivKey(privKey) {
-    const privKeyRegex = /^5[1-9A-HJ-NP-Za-km-z]{50}$/;
-    return typeof privKey === "string" && privKey.length === 51 && privKeyRegex.test(privKey);
+    return typeof privKey === "string"
+        && privKey.length === 51
+        && /^5[1-9A-HJ-NP-Za-km-z]{50}$/.test(privKey);
 }
