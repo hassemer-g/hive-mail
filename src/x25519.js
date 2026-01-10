8524460b8f9cfacd771f83771e47fc217a76f814
hassemer-g <g.hassemer@protonmail.com>
2026-01-10 17:56:13 -0400

    Update x25519.js

--- src/x25519.js
+++ src/x25519.js
@@ -1,30 +1,22 @@
 import { x25519 } from "./noble-curves/ed25519.mjs";
 
 export function createX25519KeyPair() {
-
     const { secretKey, publicKey } = x25519.keygen();
-
     return { privKey: secretKey, pubKey: publicKey };
 }
 
 export function getX25519PubKey(privKey) {
-
     const pubKey = x25519.getPublicKey(privKey);
-
     return pubKey;
 }
 
 export function buildX25519SharedSecret(pubKey) {
-
     const ephemeralPair = x25519.keygen();
     const sharedSecret = x25519.getSharedSecret(ephemeralPair.secretKey, pubKey);
-
     return { sharedSecret, encryptedSharedSecret: ephemeralPair.publicKey };
 }
 
 export function retrieveX25519SharedSecret(privKey, ephemeral) {
-
     const sharedSecret = x25519.getSharedSecret(privKey, ephemeral);
-
     return sharedSecret;
 }
